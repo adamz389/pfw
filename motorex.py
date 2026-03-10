28de8bec9c99adede6caa54413b2b52ca5abe5ec
adam <adamzidani.az@gmail.com>
2026-03-10 11:42:27 -0400

    working rpm calculations
    pid next

--- motorex.py
+++ motorex.py
@@ -1,3 +1,12 @@
+# # # ViperIDE - MicroPython Web IDE
+# # # Read more: https://github.com/vshymanskyy/ViperIDE
+
+# # # Connect your device and start creating! 🤖👨‍💻🕹️
+
+# # # You can also open a virtual device and explore some examples:
+# # # https://viper-ide.org?vm=1
+
+
 # ViperIDE - MicroPython Web IDE
 # Read more: https://github.com/vshymanskyy/ViperIDE
 
@@ -9,7 +18,163 @@
 from machine import PWM, Pin
 import time
 import math
+import gc
+import network
+import socket
 
+MAX_DUTY = 65535
+
+class Connection:
+    
+    def __init__(self, essid, password):
+        gc.collect()
+        self.ap = network.WLAN(network.AP_IF)
+        self.command_map = {}
+        self.s = None
+        self.essid = essid
+        self.password = password
+
+    def create(self):
+        self.ap.active(True)
+        self.ap.config(self.essid, self.password)
+        while not self.ap.active():
+            time.sleep(0.1)
+        time.sleep(1)
+        addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+        self.s = socket.socket()
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(addr)
+        self.s.listen(1)
+        print("Connect at http://192.168.4.1/")
+
+    def addMapping(self, cmd, function):
+        self.command_map[cmd] = function
+
+    def runMapping(self, cmd):
+        if cmd in self.command_map:
+            self.command_map[cmd]()
+        else:
+            print(f"Command {cmd} has no function")
+
+    def update(self, dt):
+        # Non-blocking check for new client
+        try:
+            self.s.settimeout(0)  # Non-blocking
+            cl, addr = self.s.accept()
+            try:
+                cmd = cl.recv(1024).decode()
+                path = cmd.split("\n")[0].split()[1].lstrip('/')
+                if path:
+                    self.runMapping(path)
+                cl.send(b"OK")
+            except OSError:
+                pass
+            finally:
+                cl.close()
+        except OSError:
+            pass
+
+class Robot:
+    
+    def __init__(self):
+        self.periodics = []
+        self.loop = Loop()
+        self.runtime = Runtime()
+        self.runtime.start()
+        self.running = True
+
+    def register(self, device):
+        if hasattr(device, "update"):
+            self.periodics.append(device.update)
+
+    def periodic(self):
+        dt = self.get_dt()
+        for func in self.periodics:
+            func(dt)
+
+    def get_runtime(self):
+        return self.runtime.elapsed()
+
+    def get_dt(self):
+        return self.loop.dt()
+
+    def is_running(self):
+        return self.running
+
+    def stop(self):
+        self.running = False
+
+class Runtime:
+
+    def __init__(self):
+        self.last_time = None
+
+    def start(self):
+        self.last_time = time.ticks_ms()
+
+    def reset(self):
+        self.last_time = time.ticks_ms()
+
+    def elapsed(self):
+        if self.last_time is None:
+            return 0
+        now = time.ticks_ms()
+        return time.ticks_diff(now, self.last_time)
+
+class Loop:
+
+    def __init__(self):
+        self.last_time = time.ticks_ms()
+
+    def dt(self):
+        now = time.ticks_ms()
+        dt = time.ticks_diff(now, self.last_time) / 1000
+        self.last_time = now
+        return max(dt, 0.001)
+
+class Utils:
+
+    @staticmethod
+    def Clamp(num, minimum, maximum):
+        return min(maximum, max(minimum, num))
+
+class Motor:
+
+    def __init__(self, m1, m2):
+        motorA1 = PWM(Pin(m1))
+        motorA2 = PWM(Pin(m2))
+
+        motorA1.freq(1000)
+        motorA2.freq(1000)
+
+        self.motorA2 = motorA2
+        self.motorA1 = motorA1
+
+
+    def setPower(self, power):
+        power = Utils.Clamp(power, -1, 1)
+
+        if power > 0:
+            self.motorA1.duty_u16(int(power * MAX_DUTY))
+            self.motorA2.duty_u16(0)
+        elif power < 0:
+            self.motorA2.duty_u16(int(-power * MAX_DUTY))
+            self.motorA1.duty_u16(0)
+        else:
+            self.stop()
+
+    def stop(self):
+        self.motorA1.duty_u16(0)
+        self.motorA2.duty_u16(0)
+
+    def setMaxDuty(self, duty):
+        MAX_DUTY = duty
+
+    def forward(self, power=1):
+        self.setPower(abs(power))
+
+    def reverse(self, power=1):
+        self.setPower(-abs(power))
 
 class PID:
 
@@ -21,14 +186,16 @@ class PID:
         self.last_error = 0
         self.last_time = time.ticks_ms()
 
-    def compute(self, target, current):
+    def compute(self, target, current, dt=None):
         # KP
         error = target - current
 
         # KI
         now = time.ticks_ms()
-        dt = (time.ticks_diff(now, self.last_time)) / 1000
+        actual_dt = (time.ticks_diff(now, self.last_time)) / 1000
         self.last_time = now
+        if dt is None:
+            dt = actual_dt
         self.integral += error * dt
 
         # KD
@@ -37,43 +204,62 @@ class PID:
 
         output = self.kP * error + self.kI * self.integral + self.kD * derivative
 
-        return max(0, min(65535, int(output)))
-
-
+        return int(Utils.Clamp(output, -MAX_DUTY, MAX_DUTY))
+        
 class MotorEX:
 
-    def __init__(self, m1, c1, pid, ppr=700):
+    def __init__(self, m1, m2, c1, pid, ppr=41, measure_interval=1.0):
         self.curr_rpm = 0
         self.targ_rpm = 0
         self.pulse_count = 0
         self.last_duty = 0
 
         self.motorA1 = PWM(Pin(m1))
-        self.encoder = Pin(c1, Pin.IN)
+        self.motorA2 = PWM(Pin(m2))
+        self.encoder = Pin(c1, Pin.IN, Pin.PULL_UP)
 
         self.motorA1.freq(1000)
+        self.motorA2.freq(1000)
 
         self.encoder.irq(trigger=Pin.IRQ_RISING, handler=self.on_pulse)
-        self.ppr = ppr
+
         self.pid = pid
+        self.ppr = ppr
+
+        # measurement timing
+        self.measure_interval = measure_interval
+        self.measure_timer = 0
+
+    def setPower(self, power):
+        power = Utils.Clamp(power, -1, 1)
+
+        if power > 0:
+            self.motorA1.duty_u16(int(power * MAX_DUTY))
+            self.motorA2.duty_u16(0)
+        elif power < 0:
+            self.motorA2.duty_u16(int(-power * MAX_DUTY))
+            self.motorA1.duty_u16(0)
+        else:
+            self.stop()
 
     def on_pulse(self, pin):
         self.pulse_count += 1
 
-    # replace dt how long motor has been counting pulses
-    def getRPM(self, dt=0.05):
+    def computeRPM(self, dt):
         revolutions = self.pulse_count / self.ppr
         rpm = revolutions * (60 / dt)
-        self.pulse_count = 0
+
         self.curr_rpm = rpm
-        return rpm
+        self.pulse_count = 0
 
     def setRPM(self, targetRPM):
-        # back and forwards
         self.targ_rpm = targetRPM
 
+    def getRPM(self):
+        return self.curr_rpm
+
     def getVelocity(self):
-        return self.rpmToRADS(self.getRPM())
+        return self.rpmToRADS(self.curr_rpm)
 
     def setVelocity(self, velocity):
         self.targ_rpm = self.radsToRPM(velocity)
@@ -87,30 +273,61 @@ class MotorEX:
     def getLastDuty(self):
         return self.last_duty
 
-    def update(self, dt=0.05):
-        rpm = self.getRPM(dt)
-        duty = self.pid.compute(self.targ_rpm, rpm)
-        self.motorA1.duty_u16(duty)
+    def update(self, dt):
+
+        # accumulate time for RPM measurement
+        self.measure_timer += dt
+
+        if self.measure_timer >= self.measure_interval:
+            self.computeRPM(self.measure_timer)
+            self.measure_timer = 0
+
+        # run PID using latest rpm
+        duty = self.pid.compute(self.targ_rpm, self.curr_rpm, dt)
+
+        if duty >= 0:
+            self.motorA1.duty_u16(duty)
+            self.motorA2.duty_u16(0)
+        else:
+            self.motorA1.duty_u16(0)
+            self.motorA2.duty_u16(-duty)
+
         self.last_duty = duty
 
-
 # must tune values
-pid = PID(1, 0.2, 0.5)
-motor = MotorEX(15, 13, pid)
-motor.setVelocity(150)  # 150 rad/s
+pid = PID(1, 0.2, 5)
+motor = MotorEX(3, 2, 13, pid, 51) 
 
-last_time = time.ticks_ms()
+
+robot = Robot()
+robot.register(motor)
 
 while True:
-    # compute dt internally in motor.getRPM(dt) ??
-    now = time.ticks_ms()
-    dt = time.ticks_diff(now, last_time) / 1000  # seconds
-    last_time = now
-    motor.update(dt)
+    motor.setPower(1)
+    robot.periodic()
+    print(f"RPM: {motor.getRPM()}")
 
+R = .0127 # wheel radius
+g = 9.81 # g constant
+# y = 0.5715
+y = 0.4445 # height of target
+theta = 60 # launch angle
 
+# def calculatePower(x):
+#         pow = -(2.94189e-9 * (x*x*x*x)) + (0.00000229358 * (x*x*x)) - (0.000625231 * (x*x)) + (0.0732917 * x) - 2.80291
+#         print(f"runs at {pow} power")
+#         return pow
 
+# def calculateOmega(x):
+#     denominator = x * math.sqrt(3) - y
+#     if denominator <= 0:
+#         print("Invalid input: square root would be negative or division by zero")
+#         return None
+#     omega = (1/R) * math.sqrt((2*g*(x*x)) / denominator)
+#     print(f"rad/s of {omega}")
+#     return omega
 
+# rad = calculateOmega(0.6096)
+# pow = calculatePower(rad)
 
-
-
+#add tiny 1ms delay in robot periodoc
